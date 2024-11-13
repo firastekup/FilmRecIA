9c2fab44276000b5c96b8ac05613d66e5cf9ba55
firas <houaidifiras.tekup@gmail.com>
2024-11-13 23:27:17 +0100

    quick fix

--- movies/models.py
+++ movies/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
     cin = models.CharField(max_length=20)
     phone = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
+    
 
     def __str__(self):
         return self.username
--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,21 @@
-# urls.py
-
+from django.conf.urls.static import static
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import AbonnementDeleteView, RegisterView, LoginView, FilmListView, FilmCreateView, FilmDeleteView, UserDashboardView, AdminDashboardView, AbonnementListView, AbonnementCreateView, UserListView
+
+from FilmRecommendationSysIA import settings
+from .views import (
+    AbonnementDeleteView,
+    RegisterView,
+    LoginView,
+    FilmListView,
+    FilmCreateView,
+    FilmDeleteView,
+    UserDashboardView,
+    AdminDashboardView,
+    AbonnementListView,
+    AbonnementCreateView,
+    UserListView
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -12,12 +25,14 @@ urlpatterns = [
     path('films/<int:pk>/delete/', FilmDeleteView.as_view(), name='film-delete'),
     path('abonnements/', AbonnementListView.as_view(), name='abonnement-list'),
     path('abonnements/create/', AbonnementCreateView.as_view(), name='abonnement-create'),
-      # Route pour créer un abonnement
     path('dashboard/user/', UserDashboardView.as_view(), name='user-dashboard'),
     path('dashboard/admin/', AdminDashboardView.as_view(), name='admin-dashboard'),
     path('abonnements/<int:pk>/delete/', AbonnementDeleteView.as_view(), name='abonnement-delete'),
-      path('users/', UserListView.as_view(), name='user-list'),
+    path('users/', UserListView.as_view(), name='user-list'),
     # JWT token URLs
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# Ajoutez la ligne pour gérer les fichiers médias
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
